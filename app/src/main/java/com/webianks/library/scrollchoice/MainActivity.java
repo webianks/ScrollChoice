6d0df7acb1a4e7adb729ab4464a3cfd2bb187022
webianks <webianks@gmail.com>
2017-01-19 11:36:12 +0530

    Added recyclerview

--- app/src/main/java/com/webianks/library/scrollchoice/MainActivity.java
+++ app/src/main/java/com/webianks/library/scrollchoice/MainActivity.java
@@ -2,12 +2,21 @@ package com.webianks.library.scrollchoice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+
     }
 }
